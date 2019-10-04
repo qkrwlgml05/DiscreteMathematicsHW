9d4c4e2d3a1668299dc7e4be079f2647759f0ed4
Jihee Park <park58917@gmail.com>
2019-10-04 14:42:37 +0900

    make multiple

--- fill.c
+++ fill.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void fill_matrix (FILE* file, int *N, int *M, int *num, int input[][1000]);
-void print (int input[][1000], int N, int M);
+void fill_matrix (FILE* file, int *N, int *M, int *num, int input[][1000], int input2[][1000]);
 int getSize (int input[][1000], int N, int M, int i, int j);
-void parse (int M, int N);
+int parse (int M, int N, int sol, int ***sol_mat);
+void print(int M, int N, int sol, int ***sol_mat);
+void copy (int M, int N, int input[][1000], int input2[][1000]);
 
 int main(int argc, char** argv) {
     char filename[100];
+
     
     if (argc < 4)
         scanf("%s", filename);
@@ -18,134 +20,196 @@ int main(int argc, char** argv) {
     
     //FILE* file = fopen(argv[1], "r");
     int input[1000][1000];
+    int input2[1000][1000];
     
     int N = 0, M = 0, num = 0;
     int i = 0, j = 0;
-    fill_matrix(file, &N, &M, &num, input);
+    int sol =0;
+    fill_matrix(file, &N, &M, &num, input, input2);
 
-	FILE * fp = fopen("formula", "w");
-
-    for (i = 1; i <= M; i++)
-        for (j = 1; j <= N; j++)
-			fprintf(fp, "(declare-const a%d%d Int)\n", i, j) ;
-
-	for (i = 1; i <= M; i++)
-        for (j = 1; j <= N; j++)
-			fprintf(fp, "(assert (and (<= a%d%d 1) (<= 0 a%d%d)))\n", i, j, i, j) ;
+        
+//=================================================
+    //multiple solution을 저장하기 위한 solution array
+    int ***solution;
+    solution = (int ***)malloc(5 * sizeof(int **)); 
+    for (int  n= 0; n < 5; n++)
+    {
+        solution[n] = (int **)malloc(M * sizeof(int *));
+        for (int x = 0; x < M; x++)
+        {
+        solution[n][x] = (int *)malloc(N * sizeof(int)); 
+        }
+    }
+    for (int n = 0; n < 5; n++)
+    {
+        for (int i = 0; i < M; i++)
+        {
+        for (int j = 0; j < N; j++)
+        {
+            solution[n][i][j] = 0;
+        }
+        }
+    }
+  
+//=========================================================
+	
 
 // ---------------------------------------------------------------------- //
-    for (i = 0; i < M; i++){
-        for (j = 0; j < N; j++){
-            
-            if (input[i][j] < 0)
-                continue;
-            if ((input[i][j] == 9)  && j > 0 && j < N-1 && i > 0 && i < M-1){
-                fprintf(fp, "(assert ( and");
-                int k = 0;
-                input[i][j] = -1;
-                for (k = 0; k < 9; k++){
-                    if (j-1+k/3 >= M || i-1+k%3 >= N || j-1+k/3 < 0 || i-1+k%3 < 0)
-                        continue;
-                    fprintf(fp, " (<= 1 a%d%d) ", i+k%3, j+k/3) ;
+    
+     //multiple solution 일단 5개만 찾기....
+    while(sol <5){
+    
+        FILE * fp = fopen("formula", "w");
+        for (int k = 1; k <=5; k++)
+            for (i = 1; i <= M; i++)
+                for (j = 1; j <= N; j++)
+                    fprintf(fp, "(declare-const a%d%d%d Int)\n", k, i, j) ;
+
+        for (i = 1; i <= M; i++)
+            for (j = 1; j <= N; j++)
+                fprintf(fp, "(declare-const a%d%d Int)\n", i, j) ;
+        
+        if (sol > 0){
+            for (int k = 0; k < sol; k++)
+                for (i = 0; i < M; i++)
+                    for (j = 0; j < N; j++)
+                        fprintf(fp, "(assert (and (<= a%d%d%d %d) (<= %d a%d%d%d)))\n", k+1, i+1, j+1, solution[k][i][j], solution[k][i][j], k+1, i+1, j+1);
+        }
+
+        for (i = 1; i <= M; i++)
+            for (j = 1; j <= N; j++)
+                fprintf(fp, "(assert (and (<= a%d%d 1) (<= 0 a%d%d)))\n", i, j, i, j) ;
+        
+        for (i = 0; i < M; i++){
+            for (j = 0; j < N; j++){
+                
+                if (input[i][j] < 0)
+                    continue;
+                if ((input[i][j] == 9)  && j > 0 && j < N-1 && i > 0 && i < M-1){
+                    fprintf(fp, "(assert ( and");
+                    int k = 0;
+                    input[i][j] = -1;
+                    for (k = 0; k < 9; k++){
+                        if (j+k%3 > N || i+k/3 > M || j+k%3 <= 0 || i+k/3 <= 0)
+                            continue;
+                        fprintf(fp, " (<= 1 a%d%d) ", i+k/3, j+k%3) ;
+                    }
+                    fprintf(fp, "))\n") ;
                 }
-                fprintf(fp, "))\n") ;
-            }
-            else if (input[i][j] == 0){
-                fprintf(fp, "(assert (and ");
-                int k = 0;
-                input[i][j] = -1;
-                for (k = 0; k < 9; k++){
-                    if (j-1+k/3 >= M || i-1+k%3 >= N || j-1+k/3 < 0 || i-1+k%3 < 0)
-                        continue;
-                    fprintf(fp, " (<= a%d%d 0) ", i+k%3, j+k/3) ;
-                }
-                fprintf(fp, "))\n") ;
-            }
-            else if ((i == 0 || i == M-1) && j > 1 && j < N-1 && input[i][j] == 6){ // 위 아래 사이드
-                fprintf(fp, "(assert (and ");
-                int k = 0;
-                input[i][j] = -1;
-                for (k = 0; k < 9; k++){
-                    if (j-1+k/3 >= M || i-1+k%3 >= N || j-1+k/3 < 0 || i-1+k%3 < 0)
-                        continue;
-                    fprintf(fp, " (<= 1 a%d%d) ", i+k%3, j+k/3) ;
-                }
-                fprintf(fp, "))\n") ;
-            }
-            else if ((j == 0 || j == N-1) && i > 0 && i < M-1 && input[i][j] == 6){ // 양 옆사이드
-                fprintf(fp, "(assert (and ");
-                int k = 0;
-                input[i][j] = -1;
-                for (k = 0; k < 9; k++){
-                    if (j-1+k/3 >= M || i-1+k%3 >= N || j-1+k/3 < 0 || i-1+k%3 < 0)
-                        continue;
-                    fprintf(fp, " (<= 1 a%d%d) ", i+k%3, j+k/3) ;
-                }
-                fprintf(fp, "))\n") ;
-            }
-            else if ((i == 0 && j == 0) || (i == M-1 && j == 0) || (i == 0 && j == N-1) || (i == M-1 && j == N-1)){
-                if (input[i][j] == 4){ // 모서리
+                else if (input[i][j] == 0){
                     fprintf(fp, "(assert (and ");
                     int k = 0;
                     input[i][j] = -1;
                     for (k = 0; k < 9; k++){
-                        if (j-1+k/3 >= M || i-1+k%3 >= N || j-1+k/3 < 0 || i-1+k%3 < 0)
+                        if (j+k%3 > N || i+k/3 > M || j+k%3 <= 0 || i+k/3 <= 0)
                             continue;
-                        fprintf(fp, " (<= 1 a%d%d) ", i+k%3, j+k/3) ;
+                        fprintf(fp, " (<= a%d%d 0) ", i+k/3, j+k%3) ;
                     }
                     fprintf(fp, "))\n") ;
+                }
+                else if ((i == 0 || i == M-1) && j > 1 && j < N-1 && input[i][j] == 6){ // 위 아래 사이드
+                    fprintf(fp, "(assert (and ");
+                    int k = 0;
+                    input[i][j] = -1;
+                    for (k = 0; k < 9; k++){
+                        if (j+k%3 > N || i+k/3 > M || j+k%3 <= 0 || i+k/3 <= 0)
+                            continue;
+                        fprintf(fp, " (<= 1 a%d%d) ", i+k/3, j+k%3) ;
+                    }
+                    fprintf(fp, "))\n") ;
+                }
+                else if ((j == 0 || j == N-1) && i > 0 && i < M-1 && input[i][j] == 6){ // 양 옆사이드
+                    fprintf(fp, "(assert (and ");
+                    int k = 0;
+                    input[i][j] = -1;
+                    for (k = 0; k < 9; k++){
+                        if (j+k%3 > N || i+k/3 > M || j+k%3 <= 0 || i+k/3 <= 0)
+                            continue;
+                        fprintf(fp, " (<= 1 a%d%d) ", i+k/3, j+k%3) ;
+                    }
+                    fprintf(fp, "))\n") ;
+                }
+                else if ((i == 0 && j == 0) || (i == M-1 && j == 0) || (i == 0 && j == N-1) || (i == M-1 && j == N-1)){
+                    if (input[i][j] == 4){ // 모서리
+                        fprintf(fp, "(assert (and ");
+                        int k = 0;
+                        input[i][j] = -1;
+                        for (k = 0; k < 9; k++){
+                            if (j+k%3 > N || i+k/3 > M || j+k%3 <= 0 || i+k/3 <= 0)
+                                continue;
+                            fprintf(fp, " (<= 1 a%d%d) ", i+k/3, j+k%3) ;
+                        }
+                        fprintf(fp, "))\n") ;
 
+                    }
                 }
             }
         }
-    }
 
-    for (i = 0; i < M; i++){
-        for (j = 0; j < N; j++){
-            if (input[i][j] < 0) continue;   
-		    fprintf(fp, "(assert(<= (+ ") ;
-                for (int k = 0 ; k < 9 ; k++){
-                    if (j+k/3 > M || i+k%3 > N || j+k/3 < 1 || i+k%3 < 1)
-                        continue;
-                    fprintf(fp, "a%d%d ", i+k%3, j+k/3);
-                }
-            fprintf(fp, ") %d))\n", input[i][j]) ;
+        for (i = 0; i < M; i++){
+            for (j = 0; j < N; j++){
+                if (input[i][j] < 0) continue;   
+                fprintf(fp, "(assert(= (+ ") ;
+                    for (int k = 0 ; k < 9 ; k++){
+                        if (j+k%3 > M || i+k/3 > N || j+k%3 < 1 || i+k/3 < 1)
+                            continue;
+                        fprintf(fp, "a%d%d ", i+k/3, j+k%3);
+                    }
+                fprintf(fp, ") %d))\n", input[i][j]) ;
 
-            if (input[i][j] < 0) continue;   
-		    fprintf(fp, "(assert(>= (+ ") ;
-                for (int k = 0 ; k < 9 ; k++){
-                    if (j+k/3 > M || i+k%3 > N || j+k/3 < 1 || i+k%3 < 1)
-                        continue;
-                    fprintf(fp, "a%d%d ", i+k%3, j+k/3);
-                }
-            fprintf(fp, ") %d))\n", input[i][j]) ;
-            input[i][j] = -1;
+                input[i][j] = -1;
+            }
         }
+//===================================================
+        //그 전 solution과 겹치지 않도록
+        if (sol != 0)
+        {
+            for (int n = 0; n < sol; n++)
+            {
+                fprintf(fp, ("(assert (not (and "));
+                for (i = 0; i < M; i++)
+                {
+                	for (j = 0; j < N; j++)
+                	{
+                    	fprintf(fp, "(= a%d%d %d)", i+1, j+1, solution[n][i][j]);
+                	}
+                }
+                fprintf(fp, ")))\n");
+            }
+        }
+    //====================================================
+
+
+        fprintf(fp, "(check-sat)\n(get-model)\n") ;
+
+        fclose(fp) ;
+
+        FILE * fin = popen("z3 formula", "r") ; //FIXME
+        FILE * fps = fopen("solution", "w") ; //FIXME
+        char buf[128] ;
+        fscanf(fin, "%s %s", buf, buf) ;
+        while (!feof(fin)) {
+            fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
+            fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
+            fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
+            fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
+            fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
+        }
+        fclose(fps);
+        pclose(fin) ;
+
+        int answer = parse(M, N, sol, solution);
+        if (answer > 0)
+            sol++;
+        else
+            break;
+        copy(M, N, input, input2);
     }
 
-
-	fprintf(fp, "(check-sat)\n(get-model)\n") ;
-
-	fclose(fp) ;
-
-	FILE * fin = popen("z3 formula", "r") ; //FIXME
-    FILE * fps = fopen("solution", "w") ; //FIXME
-	char buf[128] ;
-	fscanf(fin, "%s %s", buf, buf) ;
-	while (!feof(fin)) {
-		fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
-		fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
-		fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
-		fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
-		fscanf(fin, "%s", buf) ; fprintf(fps, "%s ", buf) ;
-	}
-    fclose(fps);
-	pclose(fin) ;
-
-    parse(M, N);
+    print(M, N, sol, solution);
 }
 
-void fill_matrix (FILE* file, int *N, int *M, int *num, int input[][1000]){
+void fill_matrix (FILE* file, int *N, int *M, int *num, int input[][1000], int input2[][1000]){
     char line[500];
     int num_char = sizeof(line)/sizeof(char);
     
@@ -156,28 +220,17 @@ void fill_matrix (FILE* file, int *N, int *M, int *num, int input[][1000]){
             
             if ((line[k-1] >= '0' && line[k-1] <= '9') || line[k-1] == '?'){
                 (*num)++;
-                input[*M][(*N)++] = line[k-1]-48;
+                input[*M][*N] = line[k-1]-'0';
+                input2[*M][(*N)++] = line[k-1]-'0';
                 if (line[k-1] == 63){
                     input[*M][(*N)-1] = -1;
+                    input2[*M][(*N)-1] = -1;
                     (*num)--;
                 }
             }
         }
         (*M)++;
     }
-}
-
-void print (int input[][1000], int N, int M){
-    int i = 0, j = 0;
-
-    printf("Input---\n");
-    for (i = 0; i < M; i++){
-        for (j = 0; j < N; j++){
-            printf("%d ", input[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
 }
 
 int getSize (int input[][1000], int N, int M, int i, int j){
@@ -191,36 +244,48 @@ int getSize (int input[][1000], int N, int M, int i, int j){
     return 9;
 }
 
-void parse (int M, int N) {
+int parse (int M, int N, int sol, int ***sol_mat) {
     FILE* file = fopen("solution", "r");
     int i, j, k;
-    int board[100][100];
+    //int board[10][100][100];
 
     char b[128] = "";
     char s[128] = "";
     char t[128] = "";
 
-    for (k = 0; k < M*N; k++){
+    for (k = 0; k < M*N*sol+M*N; k++){
         fscanf(file, "%s %s %s %s %s", b, s, b, b, t);
-
-        i = (s[1] >- '0' && s[1] <= '9' && t[0] >= '0' && t[0] <= '9') ? s[1]-'0' : 0;
+        if (s[3] >= '0' && s[3] <= '9') continue;
+        i = (s[1] >= '0' && s[1] <= '9' && t[0] >= '0' && t[0] <= '9') ? s[1]-'0' : 0;
         j = (s[2] >= '0' && s[2] <= '9' && t[0] >= '0' << t[0] <= '9') ? s[2]-'0' : 0;
 
-        board[i][j] = 0;
+        sol_mat[sol][i-1][j-1] = 0;
         int a = 0;
         if (t[0] != '0')
-            board[i][j] = 1;
+            sol_mat[sol][i-1][j-1] = 1;
     }
     if (i==0 || j==0){
-        printf("no solution\n");
-        exit(-1);
+        return 0;
     }
-    else{
-        for (i = 1; i <= M; i++){
-            for (j = 1; j <= N; j++){
-                printf("%d ", board[i][j]);
+    return 1;
+}
+
+void print(int M, int N, int sol, int ***sol_mat){
+    for (int k = 0; k < sol; k++){
+        printf("\n%d-st solution \n", k+1);
+        for (int i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                printf(" %d", sol_mat[k][i][j]);
             }
             printf("\n");
+        }
+    }
+}
+
+void copy (int M, int N, int input[][1000], int input2[][1000]){
+    for (int i = 0; i < M; i++){
+        for (int j = 0; j < N; j++){
+            input[i][j] = input2[i][j];
         }
     }
 }
